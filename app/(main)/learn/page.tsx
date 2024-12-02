971a78944320f0c3b62f95ce8bef383faa309176
gulifan <gulifan@dxy.cn>
2024-12-02 23:50:35 +0800

    feat: learn page layout and header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,19 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import UserProgress from "@/components/user-progress";
+
+import Header from "./header";
+
 const LearnPage = () => {
   return ( 
-    <div>LearnPage</div> 
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress activeCourse={{title: 'spanish', imageSrc: '/es.svg'}} hearts={5} points={100} hasActiveSubscription={false}></UserProgress>
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish"></Header>
+      </FeedWrapper>
+    </div> 
   );
 }
  
---
+++ components/feed-wrapper.tsx
@@ -0,0 +1,9 @@
+const FeedWrapper = ({children}: {children: React.ReactNode}) => {
+  return ( 
+    <div className="flex-1 relative top-0 pb-10">
+      {children}
+    </div>
+   );
+}
+ 
+export default FeedWrapper;
